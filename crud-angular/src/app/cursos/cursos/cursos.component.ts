019b5dd6e4bd133605eb2b7bd80589873d2c6314
Caio Rodrigo de Oliveira <caiooliveira@pmsv.local>
2022-07-11 14:12:42 -0300

    Tabela(Algum Erro)

---
+++ crud-angular/src/app/cursos/cursos/cursos.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Curso } from '../model/curso';
+
+@Component({
+  selector: 'app-cursos',
+  templateUrl: './cursos.component.html',
+  styleUrls: ['./cursos.component.scss']
+})
+export class CursosComponent implements OnInit {
+
+  cursos: Curso[] = [
+    { _id: "1", nome: "Angular", categoria:"front-end"}
+  ];
+  displayedColumns = ['nome','categoria'];
+  constructor() {
+    // this.cursos = [];
+  }
+
+  ngOnInit(): void {
+  }
+
+}
