65c12700dc8dd0618a82c28c4be7af8a11c54f8e
caiorodriveira <rodrigocaio2775@gmail.com>
2022-07-15 11:00:07 -0300

    Aula19(Criação de formulario, estilização e associação com variaveis no .ts)

--- crud-angular/src/app/cursos/cursos.module.ts
+++ crud-angular/src/app/cursos/cursos.module.ts
@@ -1,12 +1,13 @@
-import { SharedModule } from './../shared/shared.module';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
-
-import { CursosRoutingModule } from './cursos-routing.module';
-import { CursosComponent } from './cursos/cursos.component';//imports do cursos
 import { AppMaterialModule } from '../shared/app-material/app-material.module';
-import { CursoFormComponent } from './curso-form/curso-form.component';//importando pasta de modulos criados para material
+import { SharedModule } from './../shared/shared.module';
+import { CursoFormComponent } from './curso-form/curso-form.component';
+import { CursosRoutingModule } from './cursos-routing.module';
+import { CursosComponent } from './cursos/cursos.component';
+
 
 @NgModule({
   declarations: [
@@ -18,6 +19,7 @@ import { CursoFormComponent } from './curso-form/curso-form.component';//importa
     CursosRoutingModule,
     AppMaterialModule,
     SharedModule,
+    ReactiveFormsModule,
   ]
 })
 export class CursosModule { }
